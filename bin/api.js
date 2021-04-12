31a388fb58e6e26bcf499fafd479d6a4c8da0a90
TP-AVA <tomas.pizarro.ext@kavak.com>
2021-04-12 15:36:14 -0300

    mongose connection to cluster stablished

--- bin/api.js
+++ bin/api.js
@@ -1,5 +1,18 @@
 const app = require('./../index');
-const config = require('config')
-app.listen(config.get('PORT') , () => {
-    console.log("App listening in:>" , config.get('PORT'))
-})
+const mongoose = require('mongoose')
+const config = require('config');
+
+mongoose.connect(config.get('MONGO_URI'), { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(foo => {
+        app.listen(config.get('PORT'), () => {
+            console.log("App listening in:>", config.get('PORT'))
+        })
+    })
+    .catch((e) => {
+        app.use((req, res, next) => {
+            res.status(500).json({
+                message: "API Execution failed returned a error status of : 500",
+                error: e
+            })
+        })
+    })
